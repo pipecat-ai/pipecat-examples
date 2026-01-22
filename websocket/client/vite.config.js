729b3f00b2165c6ec0dbeb497fa8a664344d6259
filipi87 <filipi87@gmail.com>
2026-01-22 11:30:36 -0300

    Starting to refactor the example to work with PCC.

--- websocket/client/vite.config.js
+++ websocket/client/vite.config.js
@@ -1,9 +1,11 @@
+/* jshint esversion: 6 */
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react-swc';
 
 export default defineConfig({
     plugins: [react()],
     server: {
+        allowedHosts: true, // Allows external connections like ngrok
         proxy: {
             // Proxy /api requests to the backend server
             '/connect': {
