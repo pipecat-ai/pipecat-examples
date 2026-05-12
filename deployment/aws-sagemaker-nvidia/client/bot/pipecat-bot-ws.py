f10df4a8feab8ccac12bf1aaa8222c9eba199885
filipi87 <filipi87@gmail.com>
2026-05-12 12:16:10 -0300

    NVidia sagemaker example.

---
+++ deployment/aws-sagemaker-nvidia/client/bot/pipecat-bot-ws.py
@@ -0,0 +1,130 @@
+#
+# Copyright (c) 2024-2026, Daily
+#
+# SPDX-License-Identifier: BSD 2-Clause License
+#
+
+
+import os
+
+from dotenv import load_dotenv
+from loguru import logger
+from pipecat.audio.vad.silero import SileroVADAnalyzer
+from pipecat.frames.frames import LLMRunFrame
+from pipecat.pipeline.pipeline import Pipeline
+from pipecat.pipeline.runner import PipelineRunner
+from pipecat.pipeline.task import PipelineParams, PipelineTask
+from pipecat.processors.aggregators.llm_context import LLMContext
+from pipecat.processors.aggregators.llm_response_universal import (
+    LLMContextAggregatorPair,
+    LLMUserAggregatorParams,
+)
+from pipecat.runner.types import RunnerArguments
+from pipecat.runner.utils import create_transport
+from pipecat.services.openai.llm import OpenAILLMService
+from pipecat.transports.base_transport import BaseTransport, TransportParams
+from pipecat.transports.daily.transport import DailyParams
+from pipecat.services.nvidia.sagemaker.stt import NvidiaSageMakerWebsocketSTTService
+from pipecat.services.nvidia.sagemaker.tts import NvidiaSageMakerWebsocketTTSService
+
+load_dotenv(override=True)
+
+# We use lambdas to defer transport parameter creation until the transport
+# type is selected at runtime.
+transport_params = {
+    "daily": lambda: DailyParams(
+        audio_in_enabled=True,
+        audio_out_enabled=True,
+    ),
+    "webrtc": lambda: TransportParams(
+        audio_in_enabled=True,
+        audio_out_enabled=True,
+    ),
+}
+
+
+async def run_bot(transport: BaseTransport, runner_args: RunnerArguments):
+    logger.info(f"Starting bot")
+
+    stt = NvidiaSageMakerWebsocketSTTService(
+        endpoint_name=os.getenv("SAGEMAKER_ASR_ENDPOINT_NAME"),
+        region=os.getenv("AWS_REGION", "us-west-2"),
+    )
+
+    llm = OpenAILLMService(
+        api_key="not_needed",
+        base_url=os.getenv("NEMOTRON_LLM_BASE_URL"),
+        model="nemotron-3-super-120b",
+        system_instruction="You are a real-time AI voice assistant. Keep responses short and sharp. Use plain speech and quick sentences. Avoid any symbols, emojis or formatting. Show usefulness in as few words as possible. Keep you response simple, maximum of 40 words each.",
+        params=OpenAILLMService.InputParams(
+            temperature=0.0,
+            extra={
+                "extra_body": {
+                    "chat_template_kwargs": {
+                        "enable_thinking": False,
+                    }
+                }
+            },
+        ),
+    )
+
+    tts = NvidiaSageMakerWebsocketTTSService(
+        endpoint_name=os.getenv("SAGEMAKER_MAGPIE_ENDPOINT_NAME"),
+        region=os.getenv("AWS_REGION", "us-west-2"),
+    )
+
+    context = LLMContext()
+    user_aggregator, assistant_aggregator = LLMContextAggregatorPair(
+        context,
+        user_params=LLMUserAggregatorParams(vad_analyzer=SileroVADAnalyzer()),
+    )
+
+    pipeline = Pipeline(
+        [
+            transport.input(),  # Transport user input
+            stt,  # STT
+            user_aggregator,  # User responses
+            llm,  # LLM
+            tts,  # TTS
+            transport.output(),  # Transport bot output
+            assistant_aggregator,  # Assistant spoken responses
+        ]
+    )
+
+    task = PipelineTask(
+        pipeline,
+        params=PipelineParams(
+            enable_metrics=True,
+            enable_usage_metrics=True,
+        ),
+        idle_timeout_secs=runner_args.pipeline_idle_timeout_secs,
+    )
+
+    @transport.event_handler("on_client_connected")
+    async def on_client_connected(transport, client):
+        logger.info(f"Client connected")
+        # Kick off the conversation.
+        context.add_message({"role": "developer", "content": "Please introduce yourself to the user."})
+        await task.queue_frames([LLMRunFrame()])
+
+    @transport.event_handler("on_client_disconnected")
+    async def on_client_disconnected(transport, client):
+        logger.info(f"Client disconnected")
+        await task.cancel()
+
+    runner = PipelineRunner(handle_sigint=runner_args.handle_sigint)
+
+    await runner.run(task)
+
+
+async def bot(runner_args: RunnerArguments):
+    """Main bot entry point compatible with Pipecat Cloud."""
+    transport = await create_transport(runner_args, transport_params)
+    await run_bot(transport, runner_args)
+
+
+if __name__ == "__main__":
+    logger.info(f"Starting bot {os.getenv("AWS_REGION", "us-west-2")}")
+    from pipecat.runner.run import main
+
+    main()
